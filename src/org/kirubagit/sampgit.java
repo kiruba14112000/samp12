816ea1816109caab11f197e772e026471141917b
kiruba14112000 <kirubalovely2@gmail.com>
2022-04-26 16:47:38 +0530

    samp12

---
+++ src/org/kirubagit/sampgit.java
@@ -0,0 +1,16 @@
+package org.kirubagit;
+
+public class sampgit {
+	
+	public void samp1() {
+		System.out.println("kiruba sample0");
+	}
+	public void samp2() {
+		System.out.println("kiruba sample1");
+	}
+	public static void main(String[] args) {
+		sampgit k=new sampgit();
+		k.samp1();
+		k.samp2();
+	}
+}
