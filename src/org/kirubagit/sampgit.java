f96f5c225261aa7fb49c5654b822ca11d5b9427e
kiruba14112000 <kirubalovely2@gmail.com>
2022-04-27 10:45:24 +0530

    hi lusu

--- src/org/kirubagit/sampgit.java
+++ src/org/kirubagit/sampgit.java
@@ -8,6 +8,9 @@ public class sampgit {
 	public void samp2() {
 		System.out.println("kiruba sample1");
 	}
+	public void master() {
+		System.out.println("kiruba");
+	}
 	public static void main(String[] args) {
 		sampgit k=new sampgit();
 		k.samp1();
